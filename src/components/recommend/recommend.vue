3a7b125b96c1a94b7348acfff73e41576ebb1bfe
lichen <xy1ona@163.com>
2018-04-13 16:26:10 +0800

    init

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    推荐页面
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
