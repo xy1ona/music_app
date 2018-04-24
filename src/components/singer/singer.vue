92972f92909b24eddb878a1a1d78d629f2d383c8
lichen <xy1ona@163.com>
2018-04-24 17:58:06 +0800

    [配置歌手页面歌手详情子路由，vuex配置]

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="singer" ref="singer">
-    <list-view :data="singers"></list-view>
-    <router-view></router-view>
+    <list-view @select="selectSinger" :data="singers"></list-view>
+    <router-view/>
   </div>
 </template>
 
@@ -27,12 +27,16 @@
       this._getSingerList()
     },
     methods: {
+      selectSinger(singer){
+        this.$router.push({
+          path:`/singer/${singer.id}`
+        })
+      },
       _getSingerList() {
         getSingerList().then((res)=>{
           if(res.code === ERR_OK) {
             this.singers =this._normalizeSinger(res.data.list);
           }else {
-
           }
         })
       },
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import fastclick from 'fastclick'
 import VueResource from 'vue-resource'
 import VueLazyload from 'vue-lazyload'
+import store from './store'
 
 import 'common/stylus/index.styl'
 Vue.use(VueResource)
@@ -19,5 +20,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
